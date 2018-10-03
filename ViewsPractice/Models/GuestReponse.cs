c46193a78abd91d9cd0b5538a76189ce3b212143
Rob Calcote <robcalcote@gmail.com>
2018-10-03 14:11:35 -0600

    Added error messages to the RSVP form if the user left a reponse part empty.

--- ViewsPractice/Models/GuestReponse.cs
+++ ViewsPractice/Models/GuestReponse.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace ViewsPractice.Models
 {
     public class GuestReponse
     {
+        // Each variable requires an input from the user and will display an error message if they do not fill a portion of the form out
+
+        [Required(ErrorMessage = "Please enter your name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
         public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please specify if you will attend")]
         public bool? WillAttend { get; set; }
 
     }
